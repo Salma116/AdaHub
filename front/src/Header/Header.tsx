924ff43e2ff2787743e4a16e28c51ccf93a099dc
Manon <manon.coulombe@octo.com>
2021-10-15 17:00:26 +0200

    install tailwind

---
+++ front/src/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+      <div>Logo</div>
+      <div>Barre de recherche</div>
+      <div>Menu déroulant user</div>
+    </div>
+  )
+}
+
+export default Header
---
+++ front/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      blue: '#1f78f5',
+      red: '#e74f3c',
+      pink: '#f9dadb',
+      yellow: '#ffcd2b',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
